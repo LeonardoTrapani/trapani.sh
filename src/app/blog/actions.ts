727aad3078c5d1accb4b2f3d628bc908aa445071
LeonardoTrapani <leonard.trapani@gmail.com>
2024-08-05 18:30:39 +0200

    fix: actually remove postmark

--- src/app/blog/actions.ts
+++ src/app/blog/actions.ts
@@ -2,29 +2,26 @@
 
 import { z } from "zod";
 import { env } from "~~/env";
-import { postmarkClient } from "~~/lib/postmark";
 
 export const subscribe = async (formData: FormData) => {
   const email = z.string().email().safeParse(formData.get("email"));
 
-  const result = await postmarkClient.sendEmailWithTemplate({
-    TemplateId: parseInt(env.POSTMARK_WELCOME_TEMPLATE_ID),
-    To: email.data,
-    From: env.SMTP_FROM,
-    TemplateModel: {},
-    Headers: [
-      {
-        // Set this to prevent Gmail from threading emails.
-        // See https://stackoverflow.com/questions/23434110/force-emails-not-to-be-grouped-into-conversations/25435722.
-        Name: "X-Entity-Ref-ID",
-        Value: new Date().getTime() + "",
-      },
-    ],
-  });
-
-  if (result.ErrorCode) {
-    return { error: "failed to subscribe", success: false };
+  if (!email.success) {
+    return { error: "invalid email", success: false };
   }
 
-  return { success: true };
+  const response = (await fetch("https://api.useplunk.com/v1/track", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${env.PLUNK_API_KEY}`,
+    },
+    body: JSON.stringify({ email: email.data, event: "user-subscribed" }),
+  })) as unknown as { success: boolean };
+
+  if (response.success) {
+    return { success: true };
+  }
+
+  return { error: "failed to subscribe", success: false };
 };
